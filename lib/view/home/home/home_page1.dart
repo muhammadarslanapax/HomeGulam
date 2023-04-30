6ceee441f896e88c5457a0d35e838b41621145dd
Muhammad Arslan <arslanapax@gmail.com>
2023-04-30 18:44:32 +0500

    ok

--- lib/view/home/home/home_page1.dart
+++ lib/view/home/home/home_page1.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/res/color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +7,20 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-backgroundColor: Colors.red,
+    return  SafeArea(
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.symmetric(horizontal: 30,vertical: 30),
+
+          child: Column(
+            children: [
+              Text("Hello Maria",style: TextStyle(color: AppColors.text_color,fontFamily: ),)
+            ],
+
+          ),
+        ),
+
+      ),
     );
   }
 }
