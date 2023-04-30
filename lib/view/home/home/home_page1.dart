1aa85880cc9f1dbb94205b5b7d1c8247686d6a4d
Muhammad Arslan <arslanapax@gmail.com>
2023-04-30 18:52:26 +0500

    ok

--- lib/view/home/home/home_page1.dart
+++ lib/view/home/home/home_page1.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_app/res/color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class Home extends StatelessWidget {
 
           child: Column(
             children: [
-              Text("Hello Maria",style: TextStyle(color: AppColors.text_color,fontFamily: ),)
+              Text("Hello Maria",style:GoogleFonts.grandHotel(color: AppColors.text_color,fontSize: 36))
             ],
 
           ),
