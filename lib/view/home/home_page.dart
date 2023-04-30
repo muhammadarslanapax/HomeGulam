b47a0e5dd374b7be5de9517f3135bf2cdc5e7b92
Muhammad Arslan <arslanapax@gmail.com>
2023-04-30 18:06:17 +0500

    bottom nav done

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../res/constants/constants.dart' as Constants;
+import 'account/account_page.dart';
+import 'category/category_page.dart';
+import 'chat/chat_app.dart';
+import 'home/home_page1.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,13 +17,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _index = 0;
+  final List screens = const [Home(), Category(), Chat(),Account()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _index = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Text("Home Page"),
+
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: AppColors.icon_color,
+
+            type: BottomNavigationBarType.fixed,
+          backgroundColor: AppColors.bottom_nav_bg_color,
+
+
+          items: const <BottomNavigationBarItem>[
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category_outlined),
+              label: 'Category',
+            ),
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble_outline),
+              label: 'Chat',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: 'Account',
+            ),
+          ],
+          currentIndex: _index,
+          selectedItemColor: AppColors.icon_select_color,
+          onTap: _onItemTapped,
         ),
+
+        body: screens[_index],
+
 
       ),
     );
