5966a1ab568f23e5e51eed2c65c7982ef0f51a12
Muhammad usman <usmanhnd4973@gmail.com>
2023-05-06 00:15:10 +0500

    ok

--- lib/res/color.dart
+++ lib/res/color.dart
@@ -26,9 +26,7 @@ class AppColors {
    static Color dim3Color = const Color(0xF5F5F5FF);
    static Color bottomnav_bg = const Color(0xFFFFF4F1);
    static Color indicator_color = const Color(0xFFDB9080);
-   static Color dim3Color = const Color(0XF5F5F5FF);
-   static Color bottomnav_bg = const Color(0xFFFFF4F1);
-   static Color indicator_color = const Color(0xFFDB9080);
+
 
 
    // bottom nav colors
@@ -46,9 +44,6 @@ class AppColors {
    static Color star_color = const Color(0xFFFDBE61);
 
    static Color box_colorlight = const Color(0xFFFFF0ED);
-
-   static Color hint_color = const Color(0XFFE47058);
-   static Color star_color = const Color(0XFFFDBE61);
    static Color box_color = const Color(0XFFFCD6CE);
 
 
