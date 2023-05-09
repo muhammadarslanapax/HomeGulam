72196a3d791b4217fb57e8f36c047ef9f1d23ad9
Muhammad usman <usmanhnd4973@gmail.com>
2023-05-09 15:45:33 +0500

    ok

--- lib/view/BuisnessStartup/shopStartupPage.dart
+++ lib/view/BuisnessStartup/shopStartupPage.dart
@@ -13,16 +13,13 @@ class ShopStartup extends StatefulWidget {
 }
 
 class _ShopStartupState extends State<ShopStartup> {
-  final buisnessName = TextEditingController();
-  final Description = TextEditingController();
   @override
   Widget build(BuildContext context) {
-
     return  Scaffold(
       body: SingleChildScrollView(
 
         child: Container(
-          margin: EdgeInsets.all(15),
+          margin: EdgeInsets.all(10),
           child: Column(
 
             children: [
@@ -67,96 +64,32 @@ class _ShopStartupState extends State<ShopStartup> {
                   ],
                 ),
               ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-
-                            Container(
-                                margin: EdgeInsets.only(bottom: 5),
-                                child: Text("Business Name",style: GoogleFonts.poppins(color: AppColors.secondaryTextColor,fontWeight: FontWeight.w500,fontSize: 16),)),
-                            SizedBox(
-                              height: 90,
-                              child: TextFormField(
-                                keyboardType: TextInputType.emailAddress,
-                                decoration: InputDecoration(
-                                  filled: true,
-                                  fillColor: AppColors.textFieldBgColor,
-                                  border: Styles().texFormStyle(),
-                                ),
-                                controller: buisnessName,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-              SizedBox(
-                width: double.infinity,
-
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Container(
-                          margin: EdgeInsets.only(bottom: 5),
-                          child: Text("Description",style: GoogleFonts.poppins(color: AppColors.secondaryTextColor,fontWeight: FontWeight.w500,fontSize: 16),)),
-                        SizedBox(
-                          height: 90,
-                          child: SingleChildScrollView(
-                            child: TextFormField(
-                              keyboardType: TextInputType.emailAddress,
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: AppColors.textFieldBgColor,
-                                border: Styles().texFormStyle(),
-                                hintText: "A little description on what your  business is about",
-                              ),
-                              maxLines: null,
-                              controller: Description,
-                            ),
-                          ),
-                        ),
-
+                      Text("Business Name",style: GoogleFonts.poppins(color: AppColors.secondaryTextColor,fontWeight: FontWeight.w500,fontSize: 16),)
                     ],
                   ),
                 ),
-
-              Container(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      height: 50,
-                      margin: const EdgeInsets.only(top: 40),
-                      width: 140,
-                      decoration: BoxDecoration(
-                          color: AppColors.introTextColor,
-                          borderRadius: BorderRadius.circular(11)),
-                      child: TextButton(
-
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text('Next',
-                                  style: TextStyle(
-                                      fontSize: 16, color: Colors.white))
-                            ],
-                          ),
-                          onPressed: () {
-                          }),
-                    ),
-                  ],
-                ),
-              )
                   ],
                 ),
               ),
 
-
+              
+            ],
+          ),
+          
+        ),
 
     )
     );
